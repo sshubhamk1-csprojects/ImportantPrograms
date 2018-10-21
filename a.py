72ff628a0fd6c997b39ef5f26da20dba7ff198f0
Shubham Kumar <41034479+sshubhamk1-csprojects@users.noreply.github.com>
2018-10-22 02:26:36 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,25 @@
+import random
+import time
+test=input()
+total=test*3
+random.seed(time.time())
+ls=[]
+p=random.randint(1,1000000000000000000)
+for i in range(test):
+	ls.append(p)
+print total
+for i in range(test,total):
+	a=random.randint(1,1000000000000000000)
+	if(a==p):
+		a=random.randint(1,1000000000000000000)	
+	ls.append(a)
+random.shuffle(ls)
+for i in ls:
+	print i,
+
+	'''
+	100 -->300
+	adf daf  dafd adfa 
+
+
+	'''
