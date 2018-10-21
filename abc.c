e068c2185d39a43d1b93054c501aa4c783cffa32
Shubham Kumar <41034479+sshubhamk1-csprojects@users.noreply.github.com>
2018-10-22 02:32:46 +0530

    Add files via upload

---
+++ abc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void){
+	clock_t start,end;
+	start=clock();
+	double count=0;
+	for(int i=0;i<100000;i++){
+		for(int j=0;j<10000;j++){
+			count--;
+		}
+		count--;
+	}
+	printf("count: %lf\n",count);
+	end=clock();
+	double total=((double)(end-start)/ CLOCKS_PER_SEC);
+	printf("total time taken: %lf\n",total);
+	return 0;
+}
