7566e7d3677b448b01d5f7c8bb95e2237336e3cc
Shubham Kumar <41034479+sshubhamk1-csprojects@users.noreply.github.com>
2018-10-23 14:00:06 +0530

    Add files via upload

---
+++ abc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main(void){
+	char word[1001];
+	printf("Enter a string: ");
+	scanf("%[^\n]s",word);
+	printf("Enter position: ");
+	int key;
+	scanf("%d",&key);
+	for(int i=0,n=strlen(word);i<n;i++)
+	{
+		if(isalpha(word[i])){
+			if(isupper(word[i])){
+				word[i]=(word[i]-'A'+key)%26+'A';
+			}
+			else{
+				word[i]=(word[i]-'a'+key)%26+'a';
+			}
+		}
+		else if(isdigit(word[i])){
+			word[i]=(word[i]-'0'+key)%10+'0';
+		}
+	}
+	printf("Encrypted String: %s\n",word);
+
+	return 0;
+}
